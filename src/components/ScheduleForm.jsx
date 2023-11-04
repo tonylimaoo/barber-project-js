caa5450e3e199419e2dc4e404c82a17e5a8b03ae
Tony Lima <tonylima98@gmail.com>
2023-11-04 05:20:10 -0300

    New Develop

--- src/components/ScheduleForm.jsx
+++ src/components/ScheduleForm.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import styles from './Styles/schedule-form.module.css'
 
-const hours = ["8:00", "8:50", "9:40", "10:30", "11:20", "12:10", "13:00", "13:50", "14:40", "15:30", "16:20", "17:10", "18:00", "18:50", "19:40", "20:30"]
+const hours = ["7:10", "8:00", "8:50", "9:40", "10:30", "11:20", "12:10", "13:00", "13:50", "14:40", "15:30", "16:20", "17:10", "18:00", "18:50", "19:40", "20:30"]
 
 export default function ScheduleForm({
     name,
