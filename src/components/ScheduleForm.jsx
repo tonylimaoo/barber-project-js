926e1c1d4d310946e6727ed803e3af2a4ffdd07d
Tony Lima <tonylima98@gmail.com>
2023-11-10 22:05:53 -0300

    a lot of changes man

--- src/components/ScheduleForm.jsx
+++ src/components/ScheduleForm.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import './Styles/schedule-form.css'
 
-const hours = ["7:10", "8:00", "8:50", "9:40", "10:30", "11:20", "12:10", "13:00", "13:50", "14:40", "15:30", "16:20", "17:10", "18:00", "18:50", "19:40", "20:30"]
-
 export default function ScheduleForm({
     name,
     cel,
@@ -20,13 +18,14 @@ export default function ScheduleForm({
     setService,
     setProfessional,
     appointmentHours,
+    hours,
     filledForm
 }) {
     const [exclusiveHours, setExclusiveHours] = useState([]);
 
     useEffect(() => {
         setExclusiveHours(hours.filter(ele => !appointmentHours.includes(ele)));
-    }, [appointmentHours])
+    }, [appointmentHours, hours])
 
     const getDate = new Date();
 
@@ -85,152 +84,176 @@ export default function ScheduleForm({
 
     }
 
-    // JSON.stringify(array.map(e => e.horarios.join())).replace(/"|'|]|\[/g, '').split(',')
-    // Código para splitar e coletar mais de uma hora
-    // Ideia são os horarios serem coletados num array
+    const handleHourChange = (e) => {
+        if (service === "Cabelo e Barba") {
 
-    const todaysDate = formatDate();
-    const todaysDatePlusSeven = dayPlusSeven();
+            const hoursSetIndex = hours.indexOf(e.target.value) + 1;
 
-    return (
-        <>
-            <section className="form-section">
-                
-                <h1 className="form-title">Marque seu horário</h1>
+            console.log(hours[hoursSetIndex])
 
-                <form className="appointment-form"
-                    onSubmit={handleSubmit}
-                >
-                    <label>
-                        <span>Nome Completo</span>
-                        {filledForm === 'filled' ? (
-                            <input
-                                type="text"
-                                name='nome'
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder='Digite seu nome completo'
-                                value={name}
-                                required
-                                disabled
-                            />
-                        ) : (
-                            <input
-                                type="text"
-                                name='nome'
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder='Digite seu nome completo'
-                                value={name}
-                                required
-                            />
-                        )}
+            if (appointmentHours.includes(hours[hoursSetIndex])) {
+                setHour([0])
+                return window.alert("Horário seguinte indisponível pois este serviço consome 1h 40min. ")
+            } else {
+                setHour(() => [
+                    e.target.value,
+                    hours[hoursSetIndex]
+                ])
+            }
 
-                    </label>
-                    <label>
-                        <span>Celular</span>
-                        {filledForm === 'filled' ? (
-                            <input
-                                type="number"
-                                name='celular'
-                                placeholder='(19) 99323-2332'
-                                onChange={(e) => setCel(e.target.value)}
-                                value={cel}
-                                required
-                                disabled
-                            />
-                        ) : (
-                            <input
-                                type="number"
-                                name='celular'
-                                placeholder='(19) 99323-2332'
-                                onChange={(e) => setCel(e.target.value)}
-                                value={cel}
-                                required
-                            />
-                        )}
-                    </label>
-                    <label>
-                        <span>Data</span>
-                        <input
-                            type="date"
-                            name='date'
-                            min={todaysDate}
-                            max={todaysDatePlusSeven}
-                            onChange={(e) => handleDateChange(e)}
-                            // onChange={(e) => setDate(e.target.value)}
-                            value={date}
-                            required
-                        />
-                    </label>
-                    <div className="service">
+        } else {
+
+                setHour(() => [e.target.value])
+            }
+
+        }
+
+        // JSON.stringify(array.map(e => e.horarios.join())).replace(/"|'|]|\[/g, '').split(',')
+        // Código para splitar e coletar mais de uma hora
+        // Ideia são os horarios serem coletados num array
+
+        const todaysDate = formatDate();
+        const todaysDatePlusSeven = dayPlusSeven();
+
+        return (
+            <>
+                <section className="form-section">
+
+                    <h1 className="form-title">Marque seu horário</h1>
+
+                    <form className="appointment-form"
+                        onSubmit={handleSubmit}
+                    >
                         <label>
-                            <span>Serviço</span>
-                            {date === "" ? (
-                                <select>
-                                    <option value="" defaultChecked>Selecione</option>
-                                </select>
-                            ) : (
-                                <select
-                                    onChange={(e) => setService(e.target.value)}
-                                    value={service}
+                            <span>Nome Completo</span>
+                            {filledForm === 'filled' ? (
+                                <input
+                                    type="text"
+                                    name='nome'
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder='Digite seu nome completo'
+                                    value={name}
                                     required
-                                >
-                                    <option value="" defaultChecked>Selecione</option>
-                                    <option value="Cabelo">Cabelo</option>
-                                    <option value="Barba">Barba</option>
-                                    <option value="Cabelo e Barba">Cabelo e Barba</option>
-                                    <option value="Prótese + Corte">Prótese + Corte</option>
-                                    <option value="Manutenção da Prótese">Manutenção da Prótese</option>
-                                </select>
+                                    disabled
+                                />
+                            ) : (
+                                <input
+                                    type="text"
+                                    name='nome'
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder='Digite seu nome completo'
+                                    value={name}
+                                    required
+                                />
                             )}
 
                         </label>
                         <label>
-                            <span>Barbeiro</span>
-                            {date === '' ? (
+                            <span>Celular</span>
+                            {filledForm === 'filled' ? (
+                                <input
+                                    type="number"
+                                    name='celular'
+                                    placeholder='(19) 99323-2332'
+                                    onChange={(e) => setCel(e.target.value)}
+                                    value={cel}
+                                    required
+                                    disabled
+                                />
+                            ) : (
+                                <input
+                                    type="number"
+                                    name='celular'
+                                    placeholder='(19) 99323-2332'
+                                    onChange={(e) => setCel(e.target.value)}
+                                    value={cel}
+                                    required
+                                />
+                            )}
+                        </label>
+                        <label>
+                            <span>Data</span>
+                            <input
+                                type="date"
+                                name='date'
+                                min={todaysDate}
+                                max={todaysDatePlusSeven}
+                                onChange={(e) => handleDateChange(e)}
+                                // onChange={(e) => setDate(e.target.value)}
+                                value={date}
+                                required
+                            />
+                        </label>
+                        <div className="service">
+                            <label>
+                                <span>Serviço</span>
+                                {date === "" ? (
+                                    <select>
+                                        <option value="" defaultChecked>Selecione</option>
+                                    </select>
+                                ) : (
+                                    <select
+                                        onChange={(e) => setService(e.target.value)}
+                                        value={service}
+                                        required
+                                    >
+                                        <option value="" defaultChecked>Selecione</option>
+                                        <option value="Cabelo">Cabelo</option>
+                                        <option value="Barba">Barba</option>
+                                        <option value="Cabelo e Barba">Cabelo e Barba</option>
+                                        <option value="Prótese + Corte">Prótese + Corte</option>
+                                        <option value="Manutenção da Prótese">Manutenção da Prótese</option>
+                                    </select>
+                                )}
+
+                            </label>
+                            <label>
+                                <span>Barbeiro</span>
+                                {date === '' ? (
+                                    <select>
+                                        <option value="" defaultChecked>Selecione</option>
+                                    </select>
+                                ) : (
+                                    <select
+                                        onChange={(e) => setProfessional(e.target.value)}
+                                        value={professional}
+                                        required
+                                    >
+                                        <option value="" defaultChecked>Selecione</option>
+                                        <option value="Carlos">Carlos</option>
+                                        <option value="Donizete">Donizete</option>
+                                    </select>
+                                )}
+
+                            </label>
+                        </div>
+                        <label>
+                            <span>Horário</span>
+                            {professional === "" ? (
                                 <select>
-                                    <option value="" defaultChecked>Selecione</option>
+                                    <option value="" defaultValue=''>Selecione</option>
                                 </select>
                             ) : (
                                 <select
-                                    onChange={(e) => setProfessional(e.target.value)}
-                                    value={professional}
+                                    onChange={(e) => handleHourChange(e)}
+                                    value={hour[0]}
                                     required
                                 >
-                                    <option value="" defaultChecked>Selecione</option>
-                                    <option value="Carlos">Carlos</option>
-                                    <option value="Donizete">Donizete</option>
+                                    <option value="" defaultValue=''>Selecione</option>
+                                    {exclusiveHours.map((hour, i) => (
+                                        <option key={i} value={hour}>{hour}</option>
+                                    ))}
                                 </select>
                             )}
 
                         </label>
-                    </div>
-                    <label>
-                        <span>Horário</span>
-                        {professional === "" ? (
-                            <select>
-                                <option value="" defaultValue=''>Selecione</option>
-                            </select>
+                        {loading ? (
+                            <button disabled type='submit'>Aguarde</button>
                         ) : (
-                            <select
-                                onChange={(e) => setHour(e.target.value)}
-                                value={hour}
-                                required
-                            >
-                                <option value="" defaultValue=''>Selecione</option>
-                                {exclusiveHours.map((hour, i) => (
-                                    <option key={i} value={hour}>{hour}</option>
-                                ))}
-                            </select>
+                            <button type='submit'>AGENDAR</button>
                         )}
-
-                    </label>
-                    {loading ? (
-                        <button disabled type='submit'>Aguarde</button>
-                    ) : (
-                        <button type='submit'>AGENDAR</button>
-                    )}
-                </form>
-            </section >
-        </>
-    )
-}
+                    </form>
+                </section >
+            </>
+        )
+    }
